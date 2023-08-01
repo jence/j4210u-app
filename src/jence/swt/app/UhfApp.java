a73bda4306a1c26e0f525b58afb5d7ee9123639b
Jence <jence@jence.com>
2023-08-01 15:39:49 +0600

    EJ

--- src/jence/swt/app/UhfApp.java
+++ src/jence/swt/app/UhfApp.java
@@ -1,16 +1,16 @@
 /**
  * MIT LICENSE
  * 
- * Copyright © 2021 Jence, Ejaz Jamil.
+ * Copyright ï¿½ 2021 Jence, Ejaz Jamil.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
- * files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * files (the ï¿½Softwareï¿½), to deal in the Software without restriction, including without limitation the rights to use, copy, 
  * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
  * Software is furnished to do so, subject to the following conditions:
  * 
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * 
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+ * THE SOFTWARE IS PROVIDED ï¿½AS ISï¿½, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
@@ -44,7 +44,7 @@ import org.eclipse.swt.widgets.Text;
 
 public class UhfApp {
 	
-	public static final String VERSION = "2.1.0";
+	public static final String VERSION = "2.2.0";
 	
 	public static J4210U driver_ = new J4210U();
 	private static Shell app_;
